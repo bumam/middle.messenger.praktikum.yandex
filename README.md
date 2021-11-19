55cac3eec92dbf6b5ffe7d74195404301b68370e
Popleteeva <m.popleteeva@gmail.com>
2021-11-19 13:36:27 +0300

    some fixes

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 1. Устнаовите зависимости с помощью команды npm install
 2. Запустите статическую раздачу с помощью npm run serve
 3. Для разработки npm run start
+
+figma: https://www.figma.com/file/KimJqUkjNKhHL6pvDOUAW5/Chatogram
--- server.js
+++ server.js
@@ -1,79 +1,10 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const host = 'localhost'
 const port = 3000;
-
 
 app.use(express.static(`${__dirname}/dist`));
 
-// app.get('/', (req, res) => {
-//   res.render('index')
-// })
-
-// app.get('/enter', (req, res) => {
-//   res.render('login')
-// })
-
-// app.get('/signin', (req, res) => {
-//   res.render('registration')
-// })
-
-// app.get('/profile1', (req, res) => {
-//   res.render('user-profile', {
-//     data: 'full',
-//     state: 'basic',
-//   })
-// })
-
-// app.get('/profile2', (req, res) => {
-//   res.render('user-profile', {
-//     data: 'full',
-//     state: 'save',
-//   })
-// })
-// app.get('/profile3', (req, res) => {
-//   res.render('user-profile', {
-//     data: 'password',
-//     state: 'save',
-//   })
-// })
-// app.get('/profile4', (req, res) => {
-//   res.render('user-profile', {
-//     data: 'full',
-//     state: 'basic',
-//     loadingAvatar: true
-//   })
-// })
-
-// app.get('/chats1', (req, res) => {
-//   res.render('chats')
-// })
-
-// app.get('/chats2', (req, res) => {
-//   res.render('chats')
-// })
-
-// app.get('/chats3', (req, res) => {
-//   res.render('chats')
-// })
-
-// app.get('/chats4', (req, res) => {
-//   res.render('chats')
-// })
-
-// app.get('/404', (req, res) => {
-//   res.render('fail', {errors:[
-//     {code: 404, text: 'Не туда попали'}]})
-// })
-
-// app.get('/500', (req, res) => {
-//   res.render('fail', {errors:[
-//     {code: 500, text: 'Мы уже фиксим'}]})
-// })
-
-
-app.listen(port, host, function () {
-  console.log(`Server listens http://${host}:${port}`)
-})
-
+app.listen(port, function () {
+  console.log(`Server listens on ${port}`);
+});
